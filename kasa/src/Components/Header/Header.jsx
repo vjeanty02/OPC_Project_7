f5973a45b1cd3d17cd14aba241c7743e8a64c7af
Virlus Jeanty <vjeanty02@gmail.com>
2023-04-26 14:15:34 -0500

    Update Header

--- kasa/src/Components/Header/Header.jsx
+++ kasa/src/Components/Header/Header.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import "./Header.css"
 import { NavLink } from "react-router-dom";
 
+
 function Header() {
     return (
         <header className="header">
-            <img className="header_logo"></img>
+            <img src="/logo.svg" alt="Logo" />
             <nav className="header__navbar">
-                <NavLink className="navbar_link" to="/">Accueil</NavLink>
-                <NavLink className="navbar_link" to="/about">A propos</NavLink>
+                <NavLink className="header__navbar__link" to="/">Accueil</NavLink>
+                <NavLink className="header__navbar__link" to="/about">A propos</NavLink>
             </nav>
         </header>
     )
