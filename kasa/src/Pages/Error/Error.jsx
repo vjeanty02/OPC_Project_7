9f85e5da1629af4f67f8eb606c48733432acdfa0
Virlus Jeanty <vjeanty02@gmail.com>
2023-04-26 22:28:59 -0500

    Update page Error

--- kasa/src/Pages/Error/Error.jsx
+++ kasa/src/Pages/Error/Error.jsx
@@ -5,10 +5,10 @@ import { Link } from "react-router-dom";
 
 function Error() {
     return (
-        <main className="Error">
-            <h1>404</h1>
-            <p>Oups! La page que vous demandez n'existe pas.</p>
-            <Link to="/">Retourner sur la page d'accueil</Link>
+        <main className="error">
+            <h1 className="error__heading">404</h1>
+            <p className="error__paragraph">Oups! La page que vous demandez n'existe pas.</p>
+            <Link className="error__link" to="/">Retourner sur la page d'accueil</Link>
         </main>
     );
 }
