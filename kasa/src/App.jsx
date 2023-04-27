4c14d5ba21409280a542c00ec8a65169074a300e
Virlus Jeanty <vjeanty02@gmail.com>
2023-04-26 23:42:28 -0500

    Page adjustment

--- kasa/src/App.jsx
+++ kasa/src/App.jsx
@@ -3,13 +3,16 @@ import AppRouter from "./AppRouter";
 import Header from "./Components/Header/Header";
 import Footer from './Components/Footer/Footer';
 
+import "./App.css"
+
+
 function App() {
   return (
-    <div className="App">
+    <>
       <Header />
       <AppRouter />
       <Footer />
-    </div>
+    </>
   );
 }
 
