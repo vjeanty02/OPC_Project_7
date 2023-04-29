8c773905829665350841e5b06bcbd4320875f96d
Virlus Jeanty <vjeanty02@gmail.com>
2023-04-29 02:46:50 -0500

    Create Card

--- kasa/src/Components/Card/Card.jsx
+++ kasa/src/Components/Card/Card.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./Card.css"
+import { Link } from "react-router-dom";
 
 function Card() {
-    return <div>Card</div>
+    return <Link className="card">
+    <div class="card__content">
+      <h3 className="card__content__title">Titre de la location</h3>
+    </div>
+  </Link>
 }
 
 export default Card;
--- kasa/src/Pages/Home/Home.jsx
+++ kasa/src/Pages/Home/Home.jsx
@@ -2,18 +2,19 @@
 import React from "react";
 import Banner from "./../../Components/Banner/Banner";
 import Card from "./../../Components/Card/Card";
+import './Home.css';
 
 function Home() {
   return (
     <main className="Home">
       <Banner txt="Chez vous, partout et ailleurs" page="home"/>
-      <div className="Container">
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
+      <div className="home__container">
+        <Card className="card"/>
+        <Card className="card"/>
+        <Card className="card"/>
+        <Card className="card"/>
+        <Card className="card"/>
+        <Card className="card"/>
       </div>
     </main>
   );
