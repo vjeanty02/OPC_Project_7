e9e4e6522d37ff156958cffda10f93bd375473c1
Virlus Jeanty <vjeanty02@gmail.com>
2023-04-29 20:06:01 -0500

    Create Dropdown

--- kasa/src/Components/Dropdown/Dropdown.jsx
+++ kasa/src/Components/Dropdown/Dropdown.jsx
@@ -1,8 +1,41 @@
 import React from "react";
 import "./Dropdown.css"
 
-function Dropdown() {
-    return <div>Dropdown</div>
+class Dropdown extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+    };
+    this.toggleDropdown = this.toggleDropdown.bind(this);
+  }
+
+  toggleDropdown() {
+    this.setState((prevState) => ({
+      open: !prevState.open,
+    }));
+  }
+
+  render() {
+    return (
+      <div className="dropdown">
+        <div className="dropdown-button" onClick={this.toggleDropdown}>
+          Fiabilité
+          <img className={`dropdown-arrow ${this.state.open ? "open" : ""}`} src="/icone_dropdown.svg" alt="icone_dropdown" />
+        </div>
+        <ul
+          className="dropdown-list"
+          style={{ display: this.state.open ? "block" : "none" }}
+        >
+          <p className="dropdown-item">
+            La bienveillance fait partie des valeurs fondatrices de Kasa.
+            Tout comportement discriminatoire ou de perturbation du voisinage
+            entraînera une exclusion de notre plateforme.
+          </p>
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default Dropdown;
