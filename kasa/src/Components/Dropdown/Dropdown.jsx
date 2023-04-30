cb3c3bd44156575d68840df0f79195926e880749
Virlus Jeanty <vjeanty02@gmail.com>
2023-04-29 20:30:26 -0500

    Use State

--- kasa/src/Components/Dropdown/Dropdown.jsx
+++ kasa/src/Components/Dropdown/Dropdown.jsx
@@ -1,41 +1,32 @@
-import React from "react";
-import "./Dropdown.css"
+import React, { useState } from "react";
+import "./Dropdown.css";
 
-class Dropdown extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      open: false,
-    };
-    this.toggleDropdown = this.toggleDropdown.bind(this);
+function Dropdown() {
+  const [open, setOpen] = useState(false);
+
+  function toggleDropdown() {
+    setOpen((prevOpen) => !prevOpen);
   }
 
-  toggleDropdown() {
-    this.setState((prevState) => ({
-      open: !prevState.open,
-    }));
-  }
-
-  render() {
-    return (
-      <div className="dropdown">
-        <div className="dropdown-button" onClick={this.toggleDropdown}>
-          Fiabilité
-          <img className={`dropdown-arrow ${this.state.open ? "open" : ""}`} src="/icone_dropdown.svg" alt="icone_dropdown" />
-        </div>
-        <ul
-          className="dropdown-list"
-          style={{ display: this.state.open ? "block" : "none" }}
-        >
-          <p className="dropdown-item">
-            La bienveillance fait partie des valeurs fondatrices de Kasa.
-            Tout comportement discriminatoire ou de perturbation du voisinage
-            entraînera une exclusion de notre plateforme.
-          </p>
-        </ul>
+  return (
+    <div className="dropdown">
+      <div className="dropdown-button" onClick={toggleDropdown}>
+        Fiabilité
+        <img
+          className={`dropdown-arrow ${open ? "open" : ""}`}
+          src="/icone_dropdown.svg"
+          alt="icone_dropdown"
+        />
       </div>
-    );
-  }
+      <ul className="dropdown-list" style={{ display: open ? "block" : "none" }}>
+        <p className="dropdown-item">
+          La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
+          comportement discriminatoire ou de perturbation du voisinage entraînera
+          une exclusion de notre plateforme.
+        </p>
+      </ul>
+    </div>
+  );
 }
 
 export default Dropdown;
