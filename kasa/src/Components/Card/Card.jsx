1b42199cae629c9a8585042765e1e03a68e05f32
Virlus Jeanty <vjeanty02@gmail.com>
2023-04-30 00:47:50 -0500

    Update home page

--- kasa/src/Components/Card/Card.jsx
+++ kasa/src/Components/Card/Card.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import "./Card.css"
 import { Link } from "react-router-dom";
 
-function Card() {
-    return <Link className="card">
-    <div className="card__content">
-      <h3 className="card__content__title">Titre de la location</h3>
-    </div>
-  </Link>
+function Card({ title, cover }) {
+  return (
+    <Link className="card">
+      <div className="card__content">
+        <h3 className="card__content__title">{title}</h3>
+      </div>
+      <img className="card__image" src={cover} alt={title} />
+    </Link>
+  );
 }
 
 export default Card;
