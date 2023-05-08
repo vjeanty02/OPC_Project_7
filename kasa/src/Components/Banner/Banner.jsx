13918621ba59c244a12fa89fd5d0e5dc52d8408b
Virlus Jeanty <vjeanty02@gmail.com>
2023-05-08 02:46:36 -0500

    Fix: text and image in banner

--- kasa/src/Components/Banner/Banner.jsx
+++ kasa/src/Components/Banner/Banner.jsx
@@ -2,9 +2,16 @@ import React from "react";
 import "./Banner.css"
 
 function Banner({page,txt}) {
+    const words = txt.split(",");
+    const firstPart = words[0];
+    const lastPart = words[words.length - 1];
+
     return(
-        <section className={"banner banner--" + page}> 
-            <p className="banner__txt">{txt}</p>
+        <section className={"banner banner--mobile banner--" + page}> 
+            <div>
+                <span className="banner__txt">{firstPart + ","}</span>
+                <span className="banner__txt">{lastPart}</span>
+            </div>
         </section>
     );
 }
